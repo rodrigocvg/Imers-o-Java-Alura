bdeac6ee38d93955e67fc1a06163cdcf0693548f
rodrigocvg <rod300603@gmail.com>
2022-08-19 13:02:58 -0300

    enviando folder

---
+++ src/App.java
@@ -0,0 +1,49 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+
+        // Fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060"; //"https://imdb-api.com/en/API/Top250Movies/k_12345678" //
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient(); //OU HttpClient client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        //System.out.println(body);   
+
+        //Extrair só os dados que interessam(título,poster,classificação)
+        var parser = new JsonParser();
+        List<Map<String,String>> listaDeFilmes = parser.parse(body);
+        
+        //Exibir e manipuular dados
+
+        var geradora = new GeradoraDeFigurinhas();
+        for(Map<String,String> filme : listaDeFilmes){
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            System.out.println(filme.get("title"));
+            System.out.println(urlImagem);
+            //System.out.println(filme.get("imDbRating"));
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+        }
+
+    }
+}
